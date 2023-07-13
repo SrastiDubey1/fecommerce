79799f84de344722aecc64ecc320391946bbfe4e
Yash Rao <yash.dollop@gmail.com>
2023-07-13 10:57:26 +0530

    css code

--- src/Components/UI/Col/Col.jsx
+++ src/Components/UI/Col/Col.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import classes from './col-module.css'
 export default function Col() {
     return (
         <div className={`${classes['col']} ${classes[props.ContClass]}`}>{props.Children}</div>
